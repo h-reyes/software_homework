ed1c7763e2c2e07a827884c35dfd2257e1a4c10f
h-reyes <reyes.haley23@gmail.com>
2023-11-26 20:10:48 -0500

    created farm_driver.js and completed farm portion

---
+++ farm_driver.js
@@ -0,0 +1,16 @@
+let animal = 'cow';
+
+if (animal === 'cow'){
+    console.log('mooo')
+} else{
+    console.log(`Hey! You're not a cow....`)
+}
+
+animal = 'cat';
+
+if (animal === 'cow'){
+    console.log('mooo')
+} else{
+    console.log(`Hey! You're not a cow....`)
+}
+
