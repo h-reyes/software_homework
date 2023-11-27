9e8922e06d0d9a11c868c8f12c018e013a3fb730
h-reyes <reyes.haley23@gmail.com>
2023-11-27 08:50:18 -0500

    loop creation

---
+++ loop.js
@@ -0,0 +1,21 @@
+// A. The basics
+// Write a loop that will print out all the numbers from 0 to 10, inclusive
+for(let i = 0; i <= 10; i++)
+console.log(i)
+// Write a loop that will print out all the numbers from 10 up to and including 400
+for(let j = 10; j <= 400; j+= 10)
+console.log([j])
+// Write a loop that will print out every third number starting with 12 and going no higher than 4000
+for(let k = 12; k <= 4000; k += 3){
+    console.log([k])
+}
+// B. Get even students should use modulus
+// Print out the numbers that are within the range of 1 - 100
+// Adjust your code to add a message next to even numbers only that says: "<-- is an even number"
+
+for(let l = 0; l<= 100; l++){
+    console.log(l)
+}
+
+// C. Give me Five students should use modulus
+// For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five
