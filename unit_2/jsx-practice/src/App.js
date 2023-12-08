aa38b2a7b8b21d6bf71a630b3cb59748bcc77237
h-reyes <reyes.haley23@gmail.com>
2023-12-08 12:13:47 -0500

    create cart and list of items plus test code

--- unit_2/jsx-practice/src/App.js
+++ unit_2/jsx-practice/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const products = [
+  {
+    name: "allen wrench",
+    price: 2.99,
+    description: "handy tool"
+  },
+  {
+    name: "cornucopia",
+    price: 5.99,
+    description: "festive holiday decoration"
+  },
+  {
+    name: "banana",
+    price: 0.99,
+    description: "fruit of potassium"
+  },
+  {
+    name: "jack-o-lantern",
+    price: 3.99,
+    description: "spooky seasonal fun"
+  },
+  {
+    name: "doggie treat box",
+    price: 5.99,
+    description: "fido loves 'em"
+  },
+  {
+    name: "LED lightbulb",
+    price: 6.55,
+    description: "It's super efficient!"
+  },
+  {
+    name: "turtleneck",
+    price: 19.99,
+    description: "available in black!"
+  }
+]
 
 function App() {
+
+  const [cart, setCart] = useState([])
+  let test = 0
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hi There!</h1>
+      <ul>
+        {products.map(item => {
+        return (
+        <li>{item.name} <button onClick={() => {
+          setCart([...cart, item])
+        }}>Add to Cart</button></li>
+        )}
+        )}
+      </ul>
+<h2>Cart:</h2>
+<ul>
+  {cart.map(item => {
+    return(
+      <li>{item.name} - ${item.price}</li>
+    )
+  })}
+</ul>
+
+<h3>{test}</h3>
+<button onClick={() => {
+  test += 1
+  console.log(test)
+}
+}>+</button>
+
     </div>
-  );
+        );
 }
 
 export default App;
