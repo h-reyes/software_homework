ee14e7c1bd395bc6c1ac1744e2f8d440bfdc45a3
h-reyes <reyes.haley23@gmail.com>
2024-01-12 01:32:45 -0500

    completed wk08d03 lab

---
+++ unit_2/wk08d03_lab/script.js
@@ -0,0 +1,23 @@
+//1. What is a Single Page Application?
+
+// A single page application is a web application or website that users interact with by reqriting the current page rather than requesting an entirely new page from the server.
+
+//2. What is REACT?
+
+// REACT is a popular library for building user interfaces. It was developed by Facebook and was released in 2013. REACT allows developers to create interactive websites by compressing components that manage their own state and markup.
+
+//3. What is JSX?
+
+// JSX is a syntax extension for JavaScript; it allows developers to write HTML-like code within JavaScript code. Making it easier to define the structure and layout of the user interface in a declaration.
+
+//4. What is Babel?
+
+// Babel is a JavaScript compiler used to write modern JavaScript code that can still be executed in browsers and other environments that do not yet support all ES6 features.
+
+//5. What is Webpack?
+
+// Webpack is a module bundler and build tool for JS applications. Webpack can be used to bundle both JS code and other types of files like images, stylesheets and HTML files
+
+//6. What is Create React App?
+
+// Create React App is a tool that helps you start building a new single-page React application by providing a default setup. With just a single command, you can set up a new React projects.
