0e5201f6c020b91a468b8fd23ae2c20f8efd2961
h-reyes <reyes.haley23@gmail.com>
2024-01-27 02:53:39 -0500

    wk9d1 hw

---
+++ unit_2/wk9d1/day1_homework/homework.js
@@ -0,0 +1,33 @@
+// How do you make a Circle in CSS?
+// To create a circle in CSS, you can use the border-radius property and set it to 50 You also need to set a fixed and equal width and height the element.
+
+// HTML:
+// <div class="circle"></div>
+// CSS:
+// .circle { width: 100px; height:100px; border-radius: 50%; background-color: blue; }
+
+// What is Z-index?
+// Z-index is a CSS property that defines the order of overlapping HTML elements. Elements with a higher index will be placed on top of elements with a lower index.
+
+// What is a Vendor Prefix and What is it's Purpose?
+// A vendor prefix is used to add browser specific styles or properties that are not supported by all browsers. It allows developers to use new features of CSS without worrying about compatibility issues with older versions of web.
+
+// What is the difference between CSS Grid and Flexbox?
+
+/* CSS Grid vs Flexbox */
+
+// CSS Grid is a more powerful and flexible layout system that allows you to create complex and responsive designs. On the other hand, Flexbox is a simpler and more straightforward layout system that is useful for creating flexible and responsive designs. 
+
+// How might you Speed Up a Slow App? hint Google Lighthouse in Chrome Developer Tools....
+// By using Google Lighthouse the  developer tools built into chrome can be used to identify slow areas of an application and will give the dev recommendations on how to improve performance. For example Minifying code, Enabling compression, Caching, Lazy loading.
+
+// What is CRUD?
+// CRUD is an acronym that stands for Create, Read, Update, and Delete. It represents the four basic functions of persistent storage.
+
+// What is OOP?
+// OOP stands for Object Oriented Programming. The structure of object-oriented programming include the following:
+
+// Classes are user-defined data types that act as the blueprint for individual objects, attributes and methods.
+// Objects are instances of a class created with specifically defined data. Objects can correspond to real-world objects or an abstract entity. When class is defined initially, the description is the only object that is defined.
+// Methods are functions that are defined inside a class that describe the behaviors of an object. Each method contained in class definitions starts with a reference to an instance object. Additionally, the subroutines contained in an object are called instance methods. Programmers use methods for reusability or keeping functionality encapsulated inside one object at a time.
+// Attributes are defined in the class template and represent the state of an object. Objects will have data stored in the attributes field. Class attributes belong to the class itself.
