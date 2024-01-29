54f10b46296b518dd8cb36e17bb6c07dacfbc158
h-reyes <reyes.haley23@gmail.com>
2024-01-28 23:24:31 -0500

    wk9d3 lab

---
+++ unit_2/wk9d3/wk9d3_lab.js
@@ -0,0 +1,9 @@
+// What three development concepts enable the creation of comprehensive single-page applications?
+
+// Three development concepts  that are crucial for creating comprehensive single-page applications (SPAs) are: Component-Based Architecture, Client-side Routing, and State Management. 
+
+// Component-based architecture is often used when building SPAs. Components are modular, reusable, and self-contained units of code that encapsulate specific functionality or user interface elements. This approach promotes code organization, maintainability, and reusability. 
+
+// Client-side routing allows navigation within the application without triggering a full-page reload. This is crucial for SPAs, as it provides a seamless and fast user experience. The routing is typically handled on the client side, with changes to the URL triggering the display of different components or views. 
+
+// Efficient state management is essential in SPAs to maintain a consistent and reactive user interface. State refers to the data that a component needs to render and operate. Concepts like unidirectional data flow, immutability, and centralized state management (e.g., using tools like Redux or React Context) help in managing state across the application and ensuring predictable behavior.
