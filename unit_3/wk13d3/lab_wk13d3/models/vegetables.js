564004b15de952d195a5ffdc136e0ee6c811c6c3
h-reyes <reyes.haley23@gmail.com>
2024-02-07 01:04:25 -0500

    wk13d3 lab

---
+++ unit_3/wk13d3/lab_wk13d3/models/vegetables.js
@@ -0,0 +1,29 @@
+const vegetables = [
+    {
+        name:'potato',
+        color: 'brown',
+        readyToEat: true
+    },
+    {
+        name:'cucumber',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        name:'carrot',
+        color: 'orange',
+        readyToEat: true
+    },
+    {
+        name:'spinach',
+        color: 'green',
+        readyToEat: true
+    },
+    {
+        name:'eggplant',
+        color: 'purple',
+        readyToEat: true
+    }
+];
+
+module.exports= vegetables;
